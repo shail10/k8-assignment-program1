24458ccce40513bb038cdb98249db0d0bdeb652c
shail10 <shailkardani10@gmail.com>
2025-03-14 00:11:29 -0300

    routing error resolved

--- app.py
+++ app.py
@@ -8,52 +8,32 @@ app = Flask(__name__)
 STORAGE_DIR = "/shail_PV_dir"
 os.makedirs(STORAGE_DIR, exist_ok=True)
 
-@app.route('/',method=['GET'])
-def container_running():
-    return jsonify({"Status": "Container running"}), 200
+@app.route('/')
+def home():
+    return jsonify({"message": "Container is running."}), 200
 
-# @app.route('/store-file', methods=['POST'])
-# def store_file():
-#     data = request.get_json()
+@app.route('/store-file', methods=['POST'])
+def store_file():
+    data = request.get_json()
 
-#     if not data or 'file' not in data or 'data' not in data:
-#         return jsonify({"file": None, "error": "Invalid JSON input."}), 400
+    if not data or 'file' not in data or 'data' not in data:
+        return jsonify({"file": None, "error": "Invalid JSON input."}), 400
 
-#     file_name = data['file']
-#     file_content = data['data']
+    file_name = data['file']
+    file_content = data['data']
 
-#     if not file_name.strip():
-#         return jsonify({"file": None, "error": "Invalid JSON input."}), 400
+    if not file_name.strip():
+        return jsonify({"file": None, "error": "Invalid JSON input."}), 400
 
-#     file_path = os.path.join(STORAGE_DIR, file_name)
+    file_path = os.path.join(STORAGE_DIR, file_name)
 
-#     try:
-#         with open(file_path, 'w') as f:
-#             f.write(file_content)
+    try:
+        with open(file_path, 'w') as f:
+            f.write(file_content)
 
-#         return jsonify({"file": file_name, "message": "Success."}), 201
-#     except Exception as e:
-#         return jsonify({"file": file_name, "error": "Error while storing the file to the storage."}), 500
-
-# @app.route('/calculate', methods=['POST'])
-# def calculate():
-#     data = request.get_json()
-
-#     if not data or 'file' not in data or not data['file']:
-#         return jsonify({"file": None, "error": "Invalid JSON input."}), 400
-
-#     file_name = data['file']
-#     file_path = os.path.join(STORAGE_DIR, file_name)
-
-#     # Check if the file exists in persistent volume
-#     if not os.path.exists(file_path):
-#         return jsonify({"file": file_name, "error": "File not found."}), 404
-
-#     try:
-#         response = requests.post('http://container2:7000/sum', json=data)
-#         return jsonify(response.json()), response.status_code
-#     except requests.exceptions.RequestException as error:
-#         return jsonify({'error': str(error)}), 500
+        return jsonify({"file": file_name, "message": "Success."}), 201
+    except Exception as e:
+        return jsonify({"file": file_name, "error": "Error while storing the file to the storage."}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000, debug=True)
